178bdf84f50f991283e1f10a8cdb46c2c3e2a650
Azhar Uddin <63742495+Azhar-1234@users.noreply.github.com>
2023-03-18 06:50:50 -0600

    Update array_sort.php

--- array_sort.php
+++ array_sort.php
@@ -8,13 +8,37 @@
 	// sort($b);
 	// print_r($b);
 
-	function cmpasc($element1,$element2){
-			if ($element1==$element2) {
-				return 0;
-			}return ($element1<$element2)?-1:1;
-	}
+// 	function cmpasc($element1,$element2){
+// 			if ($element1==$element2) {
+// 				return 0;
+// 			}return ($element1<$element2)?-1:1;
+// 	}
 
-	$a=$b=["a"=>1,"c"=>3,"b"=>2];
-	usort($a, "cmpasc");
-	uasort($a, "cmpasc");
-	print_r($a);
+// 	$a=$b=["a"=>1,"c"=>3,"b"=>2];
+// 	usort($a, "cmpasc");
+// 	uasort($a, "cmpasc");
+// 	print_r($a);
+// $arra1=[
+//         "jan","feb","mar","apr","may","jun","july","aug","sep","oct","nov","dec"
+//     ];
+//     $arr=[
+//         "oct"=>10,
+//         "jan"=>1,
+//         "apr"=>20,
+//         "jun"=>70,
+//         "sep"=>90,
+
+//     ];
+//     $result = array_intersect($arr,$arra1);
+//     var_dump($result);
+    // $sortedArray = []; 
+    // foreach ($arra1 as  $value) {
+    //     if(!array_key_exists($value,$arr)){
+    //         continue;
+    //     }
+    //     $sortedArray[$value] =   $arr[$value];  
+       
+    // }
+
+
+    // dd(    $sortedArray);
